703659f51acf139ecd0fba2926a295975e3db6f1
Kaique Lira <Kaiqueliira@gmail.com>
2022-08-19 15:35:43 -0300

    Criação do projeto

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+const Data = [
+  {
+    id: "1",
+    name: "kaique lira",
+    email: "kaiqueliira@gmail.com",
+    phone: "82988967163",
+    salary: "4000",
+    created_at: "2022-08-30",
+  },
+  {
+    id: "2",
+    name: "Jose lira",
+    email: "joseLira@gmail.com",
+    phone: "87988475878",
+    salary: "2000",
+    created_at: "2022-08-30",
+  },
+  {
+    id: "3",
+    name: "Joao lira",
+    email: "joaolira@gmail.com",
+    phone: "8398888888",
+    salary: "1000",
+    created_at: "2022-08-30",
+  },
+];
+
+export default Data;
