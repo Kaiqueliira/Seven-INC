fdb9c2a00be116e466d230dd3edd12d6dcc61a1c
Kaique Lira <Kaiqueliira@gmail.com>
2022-08-19 21:03:56 -0300

    Criação Pagina de Lista de Usuarios

---
+++ src/pages/ListEmployee.jsx
@@ -0,0 +1,15 @@
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function ListEmployee() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>Lista de Funcionários</div>
+      </Container>
+      <Footer />
+    </>
+  );
+}
