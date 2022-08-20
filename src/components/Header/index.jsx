5db068f8159576edad328e003697c9bba70e3017
Kaique Lira <Kaiqueliira@gmail.com>
2022-08-19 21:05:47 -0300

    criação de funcionalidades para botões

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,25 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: { display: "flex", justifyContent: "space-between" },
+}));
 export default function Header() {
+  const navigate = useNavigate();
+  const classes = useStyles();
   return (
     <AppBar color="inherit" position="relative">
       <Container maxWidth="xl">
-        <Toolbar>
+        <Toolbar className={classes.root}>
           <img src="/logo.webp" alt="brand" style={{ height: "25px" }} />
+          <Button variant="outlined" onClick={() => navigate("/")}>
+            Home
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
